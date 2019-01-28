9ae4b8a9e2f32565a1825d6a90626d8f0b746358
haro <okharatov@gmail.com>
2019-01-28 12:03:43 +0300

    Готова часть функции округления

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -5,12 +5,35 @@
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
 def my_round(number, ndigits):
-    pass
+    n_str = str(number).split('.')[0]
+
+    n_len = 0 if n_str == '0' else len(n_str)
+
+    big_number_str = str(number).replace('.', '')
+    big_number = int(big_number_str[:ndigits + len(n_str)])
+    digits = str(number).split('.')[1]
+    print(big_number_str, big_number, n_len)
+
+    if int(digits[ndigits]) < 5:
+        pass
+    else:
+        big_number += 1
+
+    #big_number / (
+
+
+    # digits = str(number).split('.')[1][:ndigits]
+    # last_digit = str(number).split('.')[1][ndigits]
+    #
+    # if last_digit < 5:
+    #     return int(n)
+
 
 
 print(my_round(2.1234567, 5))
 print(my_round(2.1999967, 5))
 print(my_round(2.9999967, 5))
+print(my_round(0.9999967, 5))
 
 
 # Задание-2:
